b10fe3808d0a4dce2f23d4ec7a0fc6ea0f58425b
BlankDev117 <mmmasent@gmail.com>
2025-12-19 17:36:42 -0500

    derp

--- src/OSK.Maui.Screens.Blazor.PopupPage/Platforms/MacCatalyst/PlatformClass1.cs
+++ src/OSK.Maui.Screens.Blazor.PopupPage/Platforms/MacCatalyst/PlatformClass1.cs
@@ -1,6 +1,7 @@
-﻿namespace OSK.Maui.Screens.Blazor.PopupPage;
-
-// All the code in this file is only included on Mac Catalyst.
-public class PlatformClass1
+﻿namespace OSK.Maui.Screens.Blazor.PopupPage
 {
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
 }
